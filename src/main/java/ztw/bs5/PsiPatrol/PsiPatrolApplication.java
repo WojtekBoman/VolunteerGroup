e9ed5598c3243973f0cf6249ef1abdb862aa25e5
WojtekBoman <wojciechboman@gmail.com>
2020-03-23 11:00:45 +0100

    Init project

---
+++ src/main/java/ztw/bs5/PsiPatrol/PsiPatrolApplication.java
@@ -0,0 +1,13 @@
+package ztw.bs5.PsiPatrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiPatrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiPatrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ztw/bs5/PsiPatrol/PsiPatrolApplicationTests.java
@@ -0,0 +1,13 @@
+package ztw.bs5.PsiPatrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsiPatrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
